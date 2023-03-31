86d8521fee4d810b8beac3a76f4e673c06269659
Fatih Furkan Usta <fatihfurkanusta@outlook.com>
2023-03-31 21:12:39 +0300

    short cuts

---
+++ src/main/java/com/furkan/utils/IntellijShortCut.java
@@ -0,0 +1,10 @@
+package com.furkan.utils;
+
+public class IntellijShortCut {
+/*    Setting ==> ctrl+alt+s
+    Alt+Ins ==> constructor, toString, getter setter
+    Ctrl+/ ==> single comment
+    Ctrl+Shift+/ ==> multiple comment*/
+
+
+}
