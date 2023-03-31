d88b00fbc7f6a0fd6f24a03f9354f7af60c2a9d8
Fatih Furkan Usta <fatihfurkanusta@outlook.com>
2023-03-31 21:12:57 +0300

    Dev Tools active

---
+++ src/main/java/com/furkan/utils/ConfigProgramming.java
@@ -0,0 +1,7 @@
+package com.furkan.utils;
+
+public class ConfigProgramming {
+    // DevTools ==> LiveReload
+    //file --> settings --> compiler --> Build project automatically (Ctrl+Alt+S)
+    //file --> Advanced Settings --> Allow Auto-make to start even... (OK clicked)
+}
