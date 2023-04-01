f1f8a42f9f7f3ba42537eaaabeb6704e0d3c06d5
Fatih Furkan Usta <fatihfurkanusta@outlook.com>
2023-04-01 22:43:43 +0300

    @GetMapping @PostMapping validation @NotEmpty @Email @Size

---
+++ src/main/java/com/furkan/dto/TeacherDto.java
@@ -0,0 +1,28 @@
+package com.furkan.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
+import javax.validation.constraints.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Log4j2
+public class TeacherDto {
+
+    private Long teacherId;
+    @NotEmpty(message = "Ad Soyad Alanını Boş Geçemezsiniz!")
+    @Size(min=1,max = 18)
+    private String teacherNameSurname;
+    @NotEmpty(message = "Email Alanını Boş Geçemezsiniz!")
+    @Email(message = "Uygun formatta mail girmediniz!")
+    private String teacherEmail;
+    @NotEmpty(message = "Şifre Alanını Boş Geçemezsiniz.")
+    @Size(min=5,max = 10)
+    private String teacherPassword;
+}
