87066c60177d2e3e8e273c1619bcee712225b9cc
Fatih Furkan Usta <fatihfurkanusta@outlook.com>
2023-04-01 17:35:29 +0300

    messages.properties and error pages update

--- src/main/java/com/furkan/controller/ThymeleafController.java
+++ src/main/java/com/furkan/controller/ThymeleafController.java
@@ -30,4 +30,11 @@ public class ThymeleafController {
         model.addAttribute("key_model2","Ben Modelden geldim.-2");
         return "thymeleaf_file/thymeleaf3";
     }
+
+    //http://localhost:8080/thymeleaf4
+    @GetMapping("/thymeleaf4")
+    public String getThymeleaf4Model(Model model){
+        model.addAttribute("key_model1","Ben Modelden geldim.");
+        return "thymeleaf4";
+    }
 }
