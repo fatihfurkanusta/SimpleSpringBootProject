7afb4ce857908a82ba0f7a75b3d6cb922d477c70
Fatih Furkan Usta <fatihfurkanusta@outlook.com>
2023-04-02 22:47:56 +0300

    EmployeeController

--- src/main/java/com/furkan/data/repository/EmployeeRepository.java
+++ src/main/java/com/furkan/data/repository/EmployeeRepository.java
@@ -1,8 +1,9 @@
 package com.furkan.data.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EmployeeRepository extends JpaRepository {
+public interface EmployeeRepository extends CrudRepository {
 }
